a4db0ea0a814c5fb86194f408b1aec337930a9e0
SANTHOSH KUMAR V <149045966+vsanthoshkumarn@users.noreply.github.com>
2025-03-19 12:00:03 +0530

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/navbar'
-import Productsection from './components/productsection'
+import Productsection from './components/Productsection'
 import Herosection from './components/herosection'
 import Header from './components/header'
 import Footersection from './components/footersection'
